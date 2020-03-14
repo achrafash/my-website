2998765c025be61b4e3b26f74bf4942e35040704
AchrafAsh <achraf.ait-sidi-hammou@ensta-paris.fr>
2020-03-14 19:42:00 +0100

    Change small layout issues

--- src/components/layout.js
+++ src/components/layout.js
@@ -75,7 +75,7 @@ const Layout = ({ children }) => {
       <footer>
         © {new Date().getFullYear()}, Built with
         {` `}
-        <a href="https://www.gatsbyjs.org">Gatsby</a>
+        <a target="_blank" href="https://www.gatsbyjs.org">Gatsby</a>
       </footer>
     </>
   )
