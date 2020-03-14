3579f6b80cdd5e15e2e4ffacd796fc215b2d729d
AchrafAsh <achraf.ait-sidi-hammou@ensta-paris.fr>
2020-03-14 20:36:15 +0100

    Set up Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,12 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'none',
+      }
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
