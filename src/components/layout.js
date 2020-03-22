d527129f2d0a1b8ce5f18473135ddffac79d8460
AchrafAsh <achraf.ait-sidi-hammou@ensta-paris.fr>
2020-03-22 19:52:16 +0100

    Completely new content

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,48 +8,6 @@ import Img from "gatsby-image"
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    {
-      avatar: file(relativePath: { eq: "avatar.png" }) {
-        childImageSharp {
-          fluid {
-            ...GatsbyImageSharpFluid_tracedSVG
-          }
-        }
-      }
-      twitter: file(relativePath: { eq: "social/twitter.png" }) {
-        childImageSharp {
-          fluid {
-            ...GatsbyImageSharpFluid_tracedSVG
-          }
-        }
-      }
-      in: file(relativePath: { eq: "social/in.png" }) {
-        childImageSharp {
-          fluid {
-            ...GatsbyImageSharpFluid_tracedSVG
-          }
-        }
-      }
-      mail: file(relativePath: { eq: "social/mail.png" }) {
-        childImageSharp {
-          fluid {
-            ...GatsbyImageSharpFluid_tracedSVG
-          }
-        }
-      }
-      github: file(relativePath: { eq: "social/github.png" }) {
-        childImageSharp {
-          fluid {
-            ...GatsbyImageSharpFluid_tracedSVG
-          }
-        }
-      }
-    }
-  `)
-
-  const [active, setActive] = useState(["active", "", ""])
-
   return (
     <>
       <Helmet>
@@ -58,67 +16,42 @@ const Layout = ({ children }) => {
           content="f7vZ7b-yhgOcB6H4OwtD1rrU7r46UE_6ith2cEKPtRI"
         />
       </Helmet>
-      <div className="container">
-        <div className="profile">
-          <div className="avatar">
-            <Img fluid={data.avatar.childImageSharp.fluid} />
-          </div>
-          <span>Achraf ASH</span>
-        </div>
-        <ul className="social">
-          <li>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://twitter.com/AchrafAsh3"
-            >
-              <Img fluid={data.twitter.childImageSharp.fluid} />
-            </a>
+      <nav className="navbar">
+        <ul className="grid-col">
+          <li className="grid-child">
+            <Link to="/">AA.</Link>
           </li>
-          <li>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.linkedin.com/in/achraf-aitsidihammou/"
-            >
-              <Img fluid={data.in.childImageSharp.fluid} />
-            </a>
+          <li className="grid-child">
+            <Link to="/">Home</Link>
           </li>
-          <li>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://github.com/achrafash/"
-            >
-              <Img fluid={data.github.childImageSharp.fluid} />
-            </a>
+          <li className="grid-child">
+            <Link to="/#work">Work</Link>
           </li>
-          <li>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="mailto:aitsidihammou.achraf@gmail.com?subject=Hello!"
-            >
-              <Img fluid={data.mail.childImageSharp.fluid} />
-            </a>
+          <li className="grid-child">
+            <Link to="/#learn">Learn</Link>
+          </li>
+          <li className="grid-child">
+            <Link to="/#read">Read</Link>
           </li>
         </ul>
-        <main>{children}</main>
-        <nav className="navigation">
-          <ul>
-            <li onClick={() => setActive(["active", "", ""])} id="active">
-              <Link to="/">About</Link>
-            </li>
-            <li onClick={() => setActive(["", "active", ""])}>
-              <Link to="/services">Services</Link>
-            </li>
-            <li onClick={() => setActive(["", "", "active"])}>
-              <Link to="/portfolio">Portfolio</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
+      </nav>
+      <main>{children}</main>
+
       <footer>
+        <ul className="grid-col">
+          <li className="grid-child">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="grid-child">
+            <Link to="/#work">Work</Link>
+          </li>
+          <li className="grid-child">
+            <Link to="/#learn">Learn</Link>
+          </li>
+          <li className="grid-child">
+            <Link to="/#read">Read</Link>
+          </li>
+        </ul>
         <small>
           © {new Date().getFullYear()} - Built with{` `}
           <a target="_blank" href="https://www.gatsbyjs.org">
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,73 +1,122 @@
-import React, { useState, useEffect, useRef } from "react"
+import React, { useState } from "react"
 import { useStaticQuery } from "gatsby"
 import Img from "gatsby-image"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-function useInterval(callback, delay) {
-  const savedCallback = useRef()
-  // Remember the latest callback.
-  useEffect(() => {
-    savedCallback.current = callback
-  }, [callback])
-  // Set up the interval.
-  useEffect(() => {
-    function tick() {
-      savedCallback.current()
-    }
-    if (delay !== null) {
-      let id = setInterval(tick, delay)
-      return () => clearInterval(id)
-    }
-  }, [delay])
-}
-
 const IndexPage = () => {
-  const text = ["Landing Page ", "Portfolio ", "Prototype "]
-  const [letter, setLetter] = useState("")
-  const [counter, setCounter] = useState(0)
-  const [index, setIndex] = useState(0)
-  const [currentText, setCurrentText] = useState("")
-
-  useInterval(() => {
-    setCurrentText(text[counter])
-    setIndex(index + 1)
-    setLetter(currentText.slice(0, index))
-    if (letter.length == currentText.length) {
-      setLetter("")
-      setCounter((counter + 1) % 3)
-      setIndex(0)
-    }
-  }, 200)
-
-  const data = useStaticQuery(graphql`
-    {
-      profile: file(relativePath: { eq: "profile.png" }) {
-        childImageSharp {
-          fluid(duotone: { highlight: "#F2C94C", shadow: "#000000" }) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-    }
-  `)
-
   return (
     <Layout>
       <SEO title="Achraf ASH" />
-      <div className="wrapper home">
-        <h2>
-          Développeur Web
-          <br />
-          Étudiant-Indépendant
-        </h2>
-        <h4>{letter}</h4>
-        <div className="profile-img">
-          <svg className="circle"></svg>
-          <Img fluid={data.profile.childImageSharp.fluid} />
+      <section id="hero">
+        <span>Bonjour.</span>
+        <h1>
+          I'm Achraf,
+          <br /> a Web Developer,
+          <br /> a Junior Entrepreneur,
+          <br /> an Engineering Student in Paris 🗼 <br />
+          all at the same time.
+        </h1>
+      </section>
+      <section id="work">
+        <h2>Works</h2>
+        <hr />
+        <div className="project" id="kindleshare">
+          <h3>003. Flips Landing Page</h3>
+          <small>- March 2020</small>
+          <p>
+            This my first real-life project. I've met two amazing students at my
+            school who are building this really cool app called Flips. The point
+            is to match like-minded students at an event of a common interst.
+            <br />
+            I immediately wanted to give them a helping-hand and the timing was
+            perfect.
+            <br /> Here is a preview of the projects:
+          </p>
+          <div className="preview-20daysofcode"></div>
+          <p>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://flipsapp.fr"
+            >
+              Flips page
+            </a>
+            .
+          </p>
         </div>
-      </div>
+        <div className="project" id="kindleshare">
+          <h3>002. #20DaysOfCodeChallenge</h3>
+          <small>- February 2020</small>
+          <p>
+            The point of this challenge is to build something every single day
+            for 20 days.
+            <br />
+            My goal is to learn lots of CSS tricks in a very short period of
+            time.
+            <br /> Here is a preview of the projects:
+          </p>
+          <div className="preview-20daysofcode"></div>
+          <p>
+            I've shared them all on{" "}
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://twitter.com/achrafash3"
+            >
+              Twitter
+            </a>
+            .
+          </p>
+        </div>
+        <div className="project" id="kindleshare">
+          <h3>001. KindleShare</h3>
+          <small>- January 2020</small>
+          <p>This is a web app to:</p>
+          <ul>
+            <li>see your Kindle ebooks</li>
+            <li>browse through your Highlights</li>
+            <li>share your thoughts and highlights</li>
+            <li>explore your most inpiring people's libraries</li>
+          </ul>
+        </div>
+      </section>
+      <section id="learn">
+        <h2>Currently Learning</h2>
+        <hr />
+        <p>
+          I'm currently hooked by Data Science. But that is what I want to
+          study. What I'm currently learning (I mean on my own) is Data
+          Visualization and Machine Learning in the browser (ie with Javascript)
+        </p>
+        <span>Here are a few recent projects I went through</span>
+        <div className="carousel" id="learn-projects">
+          <div className="project">bar chart racing</div>
+          <div className="project">
+            machine learning and data visualization (Are you here ?)
+          </div>
+          <div className="project">
+            Some world map kind of data visualization
+          </div>
+        </div>
+      </section>
+      <section id="read">
+        <h2>Currently Reading</h2>
+        <hr />
+        <div className="current-book">
+          Steal Like An Artist - Austin Kleon
+          <br />
+          Rating so far: 5/5 stars
+        </div>
+        <div className="current-book">
+          The Personal MBA
+          <br />
+          Rating so far: 2/5 stars
+        </div>
+        <Link to="/blog">Read my writings ></Link>
+      </section>
     </Layout>
   )
 }
