88f6563f53c85f1e25ff720b5c734655591814a9
AchrafAsh <achraf.ait-sidi-hammou@ensta-paris.fr>
2020-05-01 08:38:42 +0200

    Redirect on hitting an unexisting route

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,33 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness...</p>
-  </Layout>
-)
+const NotFoundPage = () => {
+  // const [timer, setTimer] = useState(2000)
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (typeof window !== "undefined") {
+        window.location = "/"
+      }
+    }, 2000)
+  }, [])
+  return (
+    <Layout>
+      <SEO title="404: Not found" />
+      <div
+        style={{
+          minHeight: "75vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <h1>NOT FOUND</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness...</p>
+      </div>
+    </Layout>
+  )
+}
 
 export default NotFoundPage
